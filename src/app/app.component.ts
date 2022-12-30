6ba15d70226bb79ac35820b5f57c477c4ef0481d
Jay <jayanth.rymec@gmail.com>
2022-12-30 00:35:00 -0500

    push some random code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import { AuthService } from 'src/app/shared/auth.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jayFire1';
   titleVariable: any
    constructor(private auth:AuthService){
+   }
 
+   ngOnInit(): void {
     this.titleVariable = this.auth.getLocal();
     console.log(this.titleVariable)
-   }
+  }
 
 }
